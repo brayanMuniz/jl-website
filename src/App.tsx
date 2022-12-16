1fc72655bcb843bb1e7082a64a9bcdf52a68a5d7
Brayan Muniz <brayanMuniz2636@gmail.com>
2022-12-16 11:03:33 -0600

    Adding all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Line from "./Line";
+import { CardDataProps } from "./CardData";
+
+// mui
+import { Typography } from "@mui/material";
+
+import testData from "./testData.json";
+interface lineData {
+  line: string;
+  cards: Array<CardDataProps> | never[];
+}
+let lyricsLines: Array<lineData | string | any> = [];
+Object.entries(testData).forEach((entry) => {
+  const [lineIdx, value] = entry;
+
+  if (typeof value == "number") {
+    // @ts-ignore
+    let temp = testData[value];
+    lyricsLines.push(temp);
+  } else if (typeof value == "object") {
+    lyricsLines.push({
+      line: value.line,
+      cards: value.cards,
+    });
+  } else lyricsLines.push("");
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {lyricsLines.map((lineData, idx) => {
+        if (lineData === "") return <br key={idx} />;
+        else if (lineData.cards.length === 0)
+          return (
+            <Typography
+              key={idx}
+              variant="h4"
+              display={"inline"}
+              sx={{ margin: 1.5 }}
+            >
+              {lineData.line}
+            </Typography>
+          );
+        else return <Line key={idx} lineData={lineData}></Line>;
+      })}
     </div>
   );
 }
