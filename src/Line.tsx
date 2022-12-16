54f743d42dd3b197d89ebb6c283090e57b970549
Brayan Muniz <brayanMuniz2636@gmail.com>
2022-12-16 11:03:13 -0600

    Line component

---
+++ src/Line.tsx
@@ -0,0 +1,45 @@
+// components
+import CardData, { CardDataProps } from "./CardData";
+
+// mui
+import { Typography } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
+
+interface LineProps {
+  lineData: {
+    line: string;
+    cards: Array<CardDataProps>;
+  };
+}
+
+function Line(props: LineProps) {
+  return (
+    <div>
+      {props.lineData.cards.map((card, idx) => (
+        <Tooltip
+          title={
+            <CardData
+              word={card.word}
+              kana={card.kana}
+              definitions={card.definitions}
+              conjugation={card.conjugation}
+            />
+          }
+          placement="bottom-end"
+          key={idx}
+        >
+          <Typography
+            variant="h4"
+            display={"inline"}
+            sx={{ textDecoration: "underline", margin: 1.5 }}
+            key={idx}
+          >
+            {card.word.replace("1. ", "")}
+          </Typography>
+        </Tooltip>
+      ))}
+    </div>
+  );
+}
+
+export default Line;
