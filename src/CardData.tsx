e8db81de09503e0b521819f1344c2e805b1607fc
Brayan Muniz <brayanMuniz2636@gmail.com>
2022-12-16 11:03:02 -0600

    CardData component

---
+++ src/CardData.tsx
@@ -0,0 +1,41 @@
+import Card from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
+
+export interface CardDataProps {
+  word: string;
+  kana: string;
+  definitions: Array<string>;
+  conjugation: [string, string | null, Array<string> | []];
+}
+
+function CardData(props: CardDataProps) {
+  let orderedDef: Array<string> = [];
+  for (let i = 0; i < props.definitions.length; i += 2) {
+    let joinedDef = props.definitions[i] + " " + props.definitions[i + 1];
+    orderedDef.push(joinedDef);
+  }
+
+  const correctWord: string = props.word.replace("1.", "");
+  const correctKana: string = props.kana.replace("1.", "");
+
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h4" component="div">
+          {correctWord}
+        </Typography>
+        <Typography color="text.secondary">{correctKana}</Typography>
+        <ul>
+          {orderedDef.map((def) => (
+            <li>
+              <Typography variant="body2">{def}</Typography>
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default CardData;
